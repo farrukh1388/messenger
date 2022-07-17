c3dd54f77e00fbdd32ac33ffac50a7a4bbc577ce
farrukh_kurbanbaev <farrukh_kurbanbaev@epam.com>
2022-07-17 15:05:13 +0500

    First test is green

--- src/main/java/org/example/service/TemplateService.java
+++ src/main/java/org/example/service/TemplateService.java
@@ -1,10 +1,21 @@
 package org.example.service;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TemplateService {
 
+    private static final String PATTERN = "#\\{(?<w>\\w+)}";
+
     public String replacePlaceholders(String template, Map<String, String> placeholders) {
+        Matcher matcher = Pattern.compile(PATTERN).matcher(template);
+
+        while (matcher.find()) {
+            String match = matcher.group(1);
+            String placeholder = placeholders.get(match);
+            template = template.replace("#{" + match + "}", placeholder);
+        }
         return template;
     }
 }
